347e085f9ba44ce4534e859260123a6ee6337ebc
Glitch (personality-quiz-starter-code) <none>
2020-08-12 19:01:53 +0000

    👂👴 Checkpoint ./script.js:20125283/2754 ./index.html:20125283/7158

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 //#TODO: Create four variables to track each possible quiz outcome
 //Rename result score after the Harry Potter houses
 var questionCount = 0;
-var result1Score = 0;
-var result2Score = 0;
-var result3Score = 0;
-var result4Score = 0;
+var gryffindorScore = 0;
+var slytherinScore = 0;
+var hufflepuffScore = 0;
+var ravenclawScore = 0;
 
 //#TODO: Use the DOM to create variables for the first quiz question.
 var q1a1 = document.getElementById("q1a1");
