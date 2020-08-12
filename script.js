212ba15b04f2ca51852877e4989dc6feb13eed10
Glitch (personality-quiz-starter-code) <none>
2020-08-12 15:19:06 +0000

    🚁🐫 Checkpoint ./script.js:20125283/1765

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Global Variables
 //This is where you will define the variables you will be using in your project.
-//#TODO: Create four variables to track each possible quiz outcome 
+//#TODO: Create four variables to track each possible quiz outcome
 //Rename result score after the Harry Potter houses
 var questionCount = 0;
 var result1Score = 0;
@@ -10,24 +10,15 @@ var result4Score = 0;
 
 //#TODO: Use the DOM to create variables for the first quiz question.
 var q1a1 = document.getElementById("q1a1");
-
-
-
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
+//#TODO: Add DOM to the rest of the answer choices for the rest of the qs.
 
 
 
 //#TODO: Add Event Listeners to your answer choice variables.
 q1a1.addEventListener("click", result1);
 
-
-
-
-
-
-
 //#TODO: Define quiz functions here
-function result1(){
-  
-  
-  
-}
+function result1() {}
